bdcd84ae83107fa9e2e432625d92f9a2d33d5f1d
Jason Conroy <jason@findingsimple.com>
2012-11-10 20:13:47 +1100

    Use hybrid core seo

--- functions.php
+++ functions.php
@@ -56,7 +56,7 @@ function base_theme_setup() {
 	add_theme_support( 'hybrid-core-shortcodes' );
 	add_theme_support( 'hybrid-core-post-meta-box' );
 	add_theme_support( 'hybrid-core-theme-settings', array( 'footer', 'about' ) );
-	//add_theme_support( 'hybrid-core-seo' );
+	add_theme_support( 'hybrid-core-seo' );
 	add_theme_support( 'hybrid-core-template-hierarchy' );
 
 	/* Add theme support for framework extensions. */
