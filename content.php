aad82378407c0ab5dbadda76519b4473fe0ee586
Jason Conroy <jason@findingsimple.com>
2015-01-26 06:16:46 +1100

    Update blog

--- content.php
+++ content.php
@@ -6,7 +6,34 @@ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename(
     die ( 'You do not have sufficient permissions to access this page!' );
 }
 
-if ( is_singular( 'post' ) ) { ?>
+if ( is_home() ) { ?>
+
+<article <?php hybrid_post_attributes(); ?>>
+
+	<header class="entry-header page-header">
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php single_post_title(); ?>"><?php single_post_title(); ?></a></h1>
+	</header><!-- .entry-header -->
+
+	<?php echo apply_atomic_shortcode( 'entry_byline', '<div class="entry-byline">' . __( 'Published by [entry-author] on [entry-published] [entry-comments-link before=" | "] [entry-edit-link before=" | "]', 'simple' ) . '</div>' ); ?>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php 
+		if ( function_exists( 'wp_link_pages_extended' ) ) {
+			wp_link_pages_extended( array( 'before' => '<div class="pagination-wrapper pagination-centered"><ul class="pagination">', 'after' => '</ul></div>', 'before_page' => '<li>', 'before_current_page' => '<li class="active">', 'after_page' => '</li>'  ) ); 
+		} else {
+			wp_link_pages();
+		}
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '[entry-terms before="Posted in " taxonomy="category"] [entry-terms before="| Tagged "] [entry-updated]', 'simple' ) . '</div>' ); ?>
+	</footer><!-- .entry-footer -->
+
+</article><!-- .hentry -->
+
+<?php } elseif ( is_singular( 'post' ) ) { ?>
 
 <article <?php hybrid_post_attributes(); ?>>
 
