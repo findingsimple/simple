a8b9890eaa93fc4009ed88f17fcf720c69044484
Jason Conroy <jason@findingsimple.com>
2015-01-26 14:18:36 +1100

    Disabled scroll to top js

--- js/simple.js
+++ js/simple.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function(){
 		} 
 
 	});
-
+/*
 	//Check to see if the window is top if not then display button
 	jQuery(window).scroll(function(){
 		if ( jQuery(this).scrollTop() > 100) {
@@ -30,6 +30,7 @@ jQuery(document).ready(function(){
 		jQuery('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
+*/
 
 });
 
